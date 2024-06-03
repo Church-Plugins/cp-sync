9964f23f9ef7568b424f9572b8089a5e0c474535
Tanner Moushey <tanner@missionlab.dev>
2024-06-02 20:13:18 -0500

    code review

--- src/admin/settings/pco/api.js
+++ src/admin/settings/pco/api.js
@@ -5,44 +5,48 @@ class Api {
 	base_url = 'https://api.planningcenteronline.com';
 
 	constructor(app_id, secret) {
-		this.app_id = app_id
-		this.secret = secret
+		this.app_id = app_id;
+		this.secret = secret;
 	}
 
 	getAllGroupTypes() {
-		return this.get('/groups/v2/group_types')
+		return this.get('/groups/v2/group_types');
 	}
-	
+
 	async get(endpoint, options = {}) {
-		const response = await fetch(this.base_url + endpoint, {
-			headers: {
-				'Authorization': `Basic ${btoa(`${this.app_id}:${this.secret}`)}`,
-				'Content-Type': 'application/json',
-				...options.headers
-			},
-			...options
-		}).then(res => res.json())
+		try {
+			const response = await fetch(this.base_url + endpoint, {
+				headers: {
+					'Authorization': `Basic ${btoa(`${this.app_id}:${this.secret}`)}`,
+					'Content-Type': 'application/json',
+					...options.headers,
+				},
+				...options,
+			}).then((res) => res.json());
 
-		if(response.errors) {
-			throw new Error(response.errors[0].detail)
+			if (response.errors) {
+				throw new Error(response.errors[0].detail);
+			}
+
+			return response;
+		} catch (error) {
+			throw new Error(`Network error: ${error.message}`);
 		}
-
-		return response
 	}
 }
 
-export default Api
+export default Api;
 
 export function useApi(path) {
 	const { authConfig } = useSelect((select) => {
 		return {
-			authConfig: select(optionsStore).getOptionGroup('pco_connect')
-		}
-	}, [])
+			authConfig: select(optionsStore).getOptionGroup('pco_connect'),
+		};
+	}, []);
 
-	if(!authConfig) {
-		return null
+	if (!authConfig) {
+		return null;
 	}
 
-	return new Api(authConfig.app_id, authConfig.secret)
+	return new Api(authConfig.app_id, authConfig.secret);
 }
